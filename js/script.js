3b04bd955728ed728f5f411735340c43e3d74250
Sicktooth <sicktooth003@gmail.com>
2025-08-03 12:38:28 +0100

    made responsive with modern look added

---
+++ js/script.js
@@ -0,0 +1,6 @@
+  // Mobile nav toggle
+            const menuToggle = document.getElementById('menu-toggle');
+            const navMenu = document.getElementById('nav-menu');
+            menuToggle?.addEventListener('click', () => {
+                navMenu.classList.toggle('hidden');
+            });
